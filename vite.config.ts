2577b11537b6f5252ef72c4844099af52e808e98
saga6569 <aristov6569@gmail.com>
2025-03-22 22:57:40 +0300

    d

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/testFrontend/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+ plugins: [preact()],
 })
